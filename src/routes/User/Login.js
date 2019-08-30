abe50b17a5dc5497d45fd33bac609f9c09adb408
icowan <solacowa@gmail.com>
2019-08-30 14:36:21 +0800

    1. add configmap path 2. remove cookie auth

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -53,7 +53,10 @@ export default class LoginPage extends Component {
       Cookie.set("namespace", namespace);
       Cookie.set("username", username);
       Cookie.set("email", email);
-      Cookie.set("authorization", token.replace("Bearer+", "Bearer "));
+      // Cookie.set("authorization", token.replace("Bearer+", "Bearer "));
+      localStorage.setItem("authorization", response.data.token)
+      localStorage.setItem("username", response.data.username)
+      localStorage.setItem("email", response.data.email)
       setAuthority("admin");
       // this.props.dispatch(routerRedux.push('/dashboard/workplace'))
       window.location.href = "/"
